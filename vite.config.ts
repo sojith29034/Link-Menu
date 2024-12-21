96528cc0b1305d60c96bbb568ed0d79a927a08e6
Revant-1 <revantshinde@example.com>
2024-12-21 14:18:02 +0530

    setup backend

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://linkmenu-backend.onrender.com',
-        changeOrigin: true,
-      },
-    },
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        }
+      }
+    }
+  };
 });
